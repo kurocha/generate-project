867663d77cbd79624caa4a1fa1147637507fe1cb
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2013-03-28 04:07:58 +1300

    Add class file generator.

--- teapot.rb
+++ teapot.rb
@@ -14,23 +14,71 @@ define_generator "project" do |generator|
 		source_path = Pathname("source/#{project_name}")
 		test_path = Pathname("test/")
 		
-		name = Teapot::Name.new(project_name)
+		name = Name.new(project_name)
+		substitutions = Substitutions.new
 		
-		substitutions = {
-			# e.g. Foo Bar, typically used as a title, directory, etc.
-			'$PROJECT_NAME' => name.text,
-			
-			# e.g. FooBar, typically used as a namespace
-			'$PROJECT_IDENTIFIER' => name.identifier,
-			
-			# e.g. foo-bar, typically used for targets, executables
-			'$PROJECT_TARGET_NAME' => name.target,
-			
-			# The user's current name:
-			'$AUTHOR_NAME' => `git config --global user.name`.chomp!
-		}
+		# e.g. Foo Bar, typically used as a title, directory, etc.
+		substitutions['PROJECT_NAME'] = name.text
+		
+		# e.g. FooBar, typically used as a namespace
+		substitutions['PROJECT_IDENTIFIER'] = name.identifier
+		
+		# e.g. foo-bar, typically used for targets, executables
+		substitutions['PROJECT_TARGET_NAME'] = name.target
+		
+		# The user's current name:
+		substitutions['AUTHOR_NAME'] = `git config --global user.name`.chomp!
 		
 		generator.copy('templates/project', '.', substitutions)
+	end
+end
+
+def scope_for_namespace(namespace)
+	open = namespace.collect{|name| "namespace #{name}\n{\n"}
+	close = namespace.collect{ "}\n" }
+	
+	return open + close
+end
+
+define_generator "class" do |generator|
+	generator.description = <<-EOF
+		Generates a basic class file in the project.
+	EOF
+	
+	generator.generate do |class_name|
+		*path, class_name = class_name.split(/::/)
+		
+		directory = Pathname('source') + path.join('/')
+		directory.mkpath
+		
+		name = Name.new(class_name)
+		substitutions = Substitutions.new
+		
+		# e.g. Foo Bar, typically used as a title, directory, etc.
+		substitutions['CLASS_NAME'] = name.identifier
+		substitutions['CLASS_FILE_NAME'] = name.identifier
+		
+		# e.g. FooBar, typically used as a namespace
+		substitutions['GUARD_NAME'] = name.macro + '_H'
+		
+		# e.g. foo-bar, typically used for targets, executables
+		substitutions['NAMESPACE'] = scope_for_namespace(path)
+		
+		# The user's current name:
+		substitutions['AUTHOR_NAME'] = `git config --global user.name`.chomp!
+		
+		if ENV.key?('PROJECT_NAME')
+			# It would be nice if this was saved somewhere:
+			substitutions['PROJECT_NAME'] = ENV['PROJECT_NAME']
+		end
+		
+		substitutions['LICENSE'] = ENV['LICENSE'] || 'MIT License'
+		
+		current_date = Time.new
+		substitutions['DATE'] = current_date.strftime("%-d/%-m/%Y")
+		substitutions['YEAR'] = current_date.strftime("%Y")
+		
+		generator.copy('templates/class', directory, substitutions)
 	end
 end
 
