82e8e9d864cb56dfac93d6fe424ebaac955a2195
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2013-05-07 22:01:22 +1200

    Update for teapot 0.8.0

--- teapot.rb
+++ teapot.rb
@@ -3,7 +3,7 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
-teapot_version "0.7"
+teapot_version "0.8.0"
 
 define_generator "project" do |generator|
 	generator.description = <<-EOF
