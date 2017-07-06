a544a589c42916630e37a89b943ab3defc7fb671
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2017-07-07 01:53:00 +1200

    Use generators package, targets.

--- teapot.rb
+++ teapot.rb
@@ -3,19 +3,21 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
-teapot_version "1.4"
+teapot_version "2.0"
 
-define_generator "project" do |generator|
-	generator.description = <<-EOF
-		Generates a basic project for use with the dream-framework.
+define_target "project" do |target|
+	target.description = <<-EOF
+		Generates a basic C++ project.
 	EOF
 	
-	generator.generate do |project_name|
-		source_path = Pathname("source/#{project_name}")
-		test_path = Pathname("test/")
+	target.depends "Generate/Copy"
+	target.provides "Generate/Initial"
+	
+	target.build do |project_name|
+		source_path = Build::Files::Directory.new(target.package.path + "templates/project")
+		substitutions = target.context.substitutions.dup
 		
 		name = Build::Name.new(project_name)
-		substitutions = Substitutions.for_context(context)
 		
 		# e.g. Foo Bar, typically used as a title, directory, etc.
 		substitutions['PROJECT_NAME'] = name.text
@@ -26,6 +28,12 @@ define_generator "project" do |generator|
 		# e.g. foo-bar, typically used for targets, executables
 		substitutions['PROJECT_TARGET_NAME'] = name.target
 		
-		generator.copy('templates/project', '.', substitutions)
+		generate source: source_path, prefix: target.context.root, substitutions: substitutions
 	end
+end
+
+define_configuration "project" do |configuration|
+	configuration.public!
+	
+	configuration.require 'generators'
 end
